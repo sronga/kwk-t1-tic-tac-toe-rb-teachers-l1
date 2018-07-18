91727c6b218825ae1150c0c9df6e19e1c2ecffd8
sronga <sr899@georgetown.edu>
2018-07-18 16:20:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,3 +42,31 @@ def position_taken?(board, index)
    false
   end
 end
+
+def position_taken?(board, index)
+    if board[index] == " "
+      false
+    elsif board[index] == ""
+      false
+    elsif board[index] == nil
+      false
+    elsif board[index] == "X"
+      true
+    elsif board[index] == "O"
+      true
+    end
+end
+
+def valid_move?(board, index)
+if position_taken?(board, index) == true
+    false
+ elsif position_taken?(board, index) == false
+   (
+    if index <= 8 && index >= 0
+      true
+    else
+      false
+    end
+    )
+  end
+end
