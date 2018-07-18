639ec9a50e7ecbdd51d29d4564df4858f59ae4b2
sronga <sr899@georgetown.edu>
2018-07-18 16:22:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,3 +87,54 @@ def current_player(board)
     "O"
   end
 end
+
+def won?(board)
+
+  WIN_COMBINATIONS.each do |winindex|
+
+  position1 = winindex[0]
+  position2 = winindex[1]
+  position3 = winindex[2]
+
+  playerposition1 = board[position1]
+  playerposition2 = board[position2]
+  playerposition3 = board[position3]
+
+  if (playerposition1 == "X" && playerposition2 == "X" && playerposition3 == "X") || (playerposition1 == "O" && playerposition2 == "O" && playerposition3 == "O")
+    return winindex
+  end
+  end
+
+  return false
+end
+
+def full?(board)
+  board.all? do |eachposition|
+    eachposition != " "
+  end
+end
+
+def draw?(board)
+  if full?(board) == false
+    return false
+  elsif full?(board) == true
+    (
+    if won?(board) == true
+      return false
+    elsif won?(board) == false
+      return true
+    end
+    )
+  end
+end
+
+def over?(board)
+return (draw?(board) || full?(board) || won?(board))
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  else return nil
+  end
+end
