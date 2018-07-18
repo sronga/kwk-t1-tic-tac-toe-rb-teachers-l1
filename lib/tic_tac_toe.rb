0559f090a60ea85f22f7171441b822ae8b7dd541
sronga <sr899@georgetown.edu>
2018-07-18 16:29:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,8 +9,6 @@ WIN_COMBINATIONS = [
   [2,5,8]
 ]
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
